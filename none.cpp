3a6ba4c50e91088942047cdb4188a23f913a99e5
Sahil Sahu <imsahilsahu11@gmail.com>
2024-05-29 00:52:04 +0530

    first

---
+++ none.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define in(A,N); ll A[N]; for(int z=0;z<N;z++){cin>>A[z];}
+#define pt(A,N); for(int z=0;z<N;z++){cout<<A[z]<<" ";} cout<<endl;
+#define all(x) x.begin(), x.end()
+#define add(a,b,c) for(int z=0;z<b;z++){c+=a[z];}
+using namespace std;
+
+
+
+
+int main()
+{
+    // int t;
+    // cin>>t;
+    // while(t--){
+    //     sol();
+    // }
+    
+}
